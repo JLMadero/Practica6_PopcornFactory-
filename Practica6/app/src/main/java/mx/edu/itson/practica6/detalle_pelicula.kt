2ee81596070fea71a72c928b6beab7cc87e3ad46
JLMadero <madero26jose08@gmail.com>
2025-03-10 21:46:02 -0700

    back

--- Practica6/app/src/main/java/mx/edu/itson/practica6/detalle_pelicula.kt
+++ Practica6/app/src/main/java/mx/edu/itson/practica6/detalle_pelicula.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.practica6
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -16,12 +18,20 @@ class detalle_pelicula : AppCompatActivity() {
         var iv_pelicula_image: ImageView = findViewById(R.id.iv_pelicula_imagen)
         var tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         var tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        var buy: Button = findViewById(R.id.buyTickets)
+        var seaLeft: TextView = findViewById(R.id.seaLeft)
 
         var bundle=intent.extras
         if (bundle!=null){
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            seaLeft.setText("20 seats available")
+        }
+
+        buy.setOnClickListener(){
+            var intent: Intent = Intent(this, SeatSelection::class.java)
+            startActivity(intent)
         }
     }
 }
